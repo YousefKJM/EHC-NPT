6b2af9f154feef5d7a5f9bde19d00e13462abe56
YousefKJM <uaso0of056@gmail.com>
2020-02-27 15:12:47 -0500

    Relational and Boolean Operators

--- python101.py
+++ python101.py
@@ -97,3 +97,10 @@ print(type(bool1))
 bool5 = "True"
 print(type(bool5))
 
+#Relational and Boolean Operators
+greater_than = 7 > 5
+less_than = 5 < 7
+greater_than_equal_to = 7 >= 7
+less_than_equal_to = 7 <= 7
+
+print(greater_than, less_than, greater_than_equal_to, less_than_equal_to)
