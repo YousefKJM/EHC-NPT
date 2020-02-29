b06d62a810e243fa03f84a4e8fa882f086a05d09
YousefKJM <uaso0of056@gmail.com>
2020-02-29 15:07:38 -0500

    def new_line func python102.py

--- python102.py
+++ python102.py
@@ -8,5 +8,11 @@ import sys #system functions and parameters
 from datetime import datetime
 print(datetime.now())
 
-from datetime import datetime as dt #import with an alias
+from datetime import datetime as dt #importing with an alias
 print(dt.now())
+
+def new_line():
+	print('\n')
+
+new_line()
+
