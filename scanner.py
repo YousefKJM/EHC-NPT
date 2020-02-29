f31021b9f1d4d8b620429bc57b56ad35bd266654
YousefKJM <uaso0of056@gmail.com>
2020-02-29 16:43:32 -0500

    add a iterate on port with try&except scanner.py

--- scanner.py
+++ scanner.py
@@ -17,3 +17,23 @@ print("-" * 50)
 print("Scanning target "+target)
 print("Time started: "+str(datetime.now()))
 print("-" * 50)
+
+try:
+	for port in range(50, 85):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefualttimeout(1) #is a float
+		result = s.connect_ex((target.port)) #return error indicator
+		print("Checking port {}".format(port))
+		if result == 0:
+			print("Port {} is open".format(port))
+		s.close()
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
+	
