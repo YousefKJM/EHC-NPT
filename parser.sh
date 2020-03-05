f4727596d2664c2a806c7615dbc43832ff3893ab
YousefKJM <uaso0of056@gmail.com>
2020-03-05 16:21:22 -0500

    finish if close parser.sh

--- parser.sh
+++ parser.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+if [ "$#" != "2" ]; then
+	echo "Breach-Parse v2: A Breached Domain Parsing Tool by Heath Adams"
+	echo " "
+	echo "Usage: breach-parse <domain to search> <file to output>"
+	echo "Example: breach-parse @gmail.com gmail.txt"
+	echo " "
+	echo 'For multiple domains: breach-parse "<domain to search>|<domain to search>" <file to output>'
+	echo 'Exmple: breach-parse "@gmail.com|@yahoo.com" multiple.txt' 
