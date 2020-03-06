146eae53d1562d5cc27f660941c5a4a3b61e2488
YousefKJM <uaso0of056@gmail.com>
2020-03-06 12:42:36 -0500

    finish half of else  clouse parser.sh

--- parser.sh
+++ parser.sh
@@ -8,3 +8,15 @@ if [ "$#" != "2" ]; then
 	echo " "
 	echo 'For multiple domains: breach-parse "<domain to search>|<domain to search>" <file to output>'
 	echo 'Exmple: breach-parse "@gmail.com|@yahoo.com" multiple.txt' 
+else 
+	fullfile=$2
+	fbname=$(basename "$fullfile" | cut -d. -f1)
+	master=$fbname-master.txt
+	users=$fbname-users.txt
+	passwords=$fbname-passwords.txt
+	
+	touch $master
+	total_Files=$(find /opt/breach-parse/BreachCompilation/data -type f | wc -l)
+	file_Count=0
+
+	
